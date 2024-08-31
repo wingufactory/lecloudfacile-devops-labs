8766a59e349f15505f766ec4dbd6b87dedeae26f
k6sy <kalidou.harouna.sy@gmail.com>
2024-08-31 14:07:55 +0200

    fix typos

--- sql-dummy-data/04-users-groups.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user_groups (
-    user_id INT,
-    group_id INT,
-    PRIMARY KEY (user_id, group_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (group_id) REFERENCES groups(group_id)
-);
