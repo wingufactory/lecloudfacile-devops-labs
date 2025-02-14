a94ee9c2816daa8c142ecb65f44ab5aff332e357
papicool <papemfall@gmail.com>
2025-02-14 21:34:11 +0100

    add stack-swarm

---
+++ 05-tests-continus/selenium-test/seleniumFramework/src/test/java/seleniumFramework/WebDriverTest.java
@@ -0,0 +1,10 @@
+package seleniumFramework;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverTest {
+    public static void main( String[] args )
+    {
+       WebDriver driver = new ChromeDriver();
+    }
+}
